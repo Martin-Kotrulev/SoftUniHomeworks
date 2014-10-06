989962bd26f11b674988f1c319a5e27d82ac23e0
Martin Kotrulev <martin.kotrulev@gmail.com>
2014-10-06 21:57:22 +0300

    Update 0.9

--- CCharp Basic/03.OperatorsExpressionsStatements/06.FourDigitNumber/FourDigitNumber.cs
+++ CCharp Basic/03.OperatorsExpressionsStatements/06.FourDigitNumber/FourDigitNumber.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 //Problem 6.	Four-Digit Number
 //Write a program that takes as input a four-digit number in format abcd (e.g. 2011) and performs the following:
 //•	Calculates the sum of the digits (in our example 2+0+1+1 = 4).
@@ -9,6 +11,26 @@
 
 class FourDigitNumber
 {
+    static int PutLastToFirst(int num)
+    {
+        int last = num % 10;
+        num /= 10;
+        last = last * 1000 + num;
+        return last;
+    }
+    //static string ExchangeSecondAndThird(string num)
+    //{
+    //    char[] number = new char[num.Length];
+    //    for (int i = 0; i < num.Length; i++)
+    //    {
+    //        if (i != 0 && i != num.Length - 1)
+    //        {
+    //            char swap = num[i];
+                
+    //        }
+    //        else number[i] = num[i];
+    //    }
+    //}
     static int ReverseNumber(int n)
     {
         int reversed = 0;
@@ -42,7 +64,8 @@ class FourDigitNumber
 
         } while (digit != 13); // 13 is ENTER
         int reversed = ReverseNumber(n);
+        Console.WriteLine(sum);
         Console.WriteLine(reversed);
-
+        Console.WriteLine(PutLastToFirst(n));
     }
 }
