f5c5a46ab7dcffd3f346a81530a867f5aff2b041
Martin Kotrulev <martin.kotrulev@gmail.com>
2014-10-22 00:06:49 +0300

    Update. 3 exam projects

--- CCharp Basic/05.ConditionalStatements/12.ZeroSubset/ZeroSubset.cs
+++ CCharp Basic/05.ConditionalStatements/12.ZeroSubset/ZeroSubset.cs
@@ -1,59 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
+//Problem 12.	* Zero Subset
+//We are given 5 integer numbers. Write a program that 
+//finds all subsets of these numbers whose sum is 0. 
+//Assume that repeating the same subset several times is not a problem. 
 
 namespace _12.ZeroSubset
 {
     class ZeroSubset
     {
-        static int[] n = new int[3];
-        static bool[] check = { false, false, false, false, false };
-        static List<int> subset = new List<int>();
+        static int[] n = { 3, -3, 2, -2, 8 };
+        // array of indexes of numbers used in subset
+        static int[] indexes = new int[5];
+        static int k; // current combination/subset count
 
-        static void PrintSubset(List<int> subset)
+        static void CheckSubset()
         {
-            for (int i = 0; i < subset.Count; i++)
+            int subsetSum = 0;
+            for (int i = 0; i <= k; i++)
             {
-                Console.Write(subset[i] +
-                    (i == subset.Count - 1 ? " = 0\n" : " + "));
+                subsetSum += n[indexes[i]];
             }
-            subset.Clear();
-        }
 
-        static void CheckSubset(List<int> subset)
-        {
-            int sum = 0;
-            foreach (var item in subset)
+            if (subsetSum == 0)
             {
-                sum += item;
-            }
-            if (sum == 0) PrintSubset(subset);
-        }
-
-        static void GetSubsetSum(int i)
-        {
-            if (i == n.Length) return;
-
-            for (int j = 0; j < n.Length; i++)
-            {
-                if (!check[j])
+                for (int i = 0; i <= k; i++)
                 {
-                    subset.Add(n[j]);
-                    check[j] = true;
+                    Console.Write(n[indexes[i]] + 
+                        (i == k ? " = 0\n" : " + "));
                 }
-                CheckSubset(subset);
-                GetSubsetSum(i + 1);
-                check[j] = false;
+            }
+        }
+
+        static void Combination(int i, int next)
+        {
+            if (i > k) return;
+
+            for (int j = next; j < n.Length; j++)
+            {
+                indexes[i] = j;
+                if (i == k) CheckSubset();
+                Combination(i + 1, j + 1);
             }
         }
 
         static void Main()
         {
-            for (int i = 0; i < n.Length; i++)
+            for (k = 0; k < n.Length; k++)
             {
-                n[i] = int.Parse(Console.ReadLine());
+                Combination(0, 0);
             }
-
-            GetSubsetSum(0);
         }
     }
 }
