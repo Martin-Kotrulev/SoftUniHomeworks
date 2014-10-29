4183b7d14f8004bb50f3c375fc1b0b7f2b1c4fd3
Martin Kotrulev <martin.kotrulev@gmail.com>
2014-10-29 20:36:45 +0200

    Update

---
+++ CCharp Basic/05.ConditionalStatements/16.MagicDates/MagicDates.cs
@@ -0,0 +1,72 @@
+﻿using System;
+//Problem 16.	** – Magic Dates
+//Consider we are given a date in format dd-mm-yyyy, e.g. 17-03-2007.
+//We calculate the weight of this date by joining together all its digits, 
+//multiplying each digit by each of the other digits and finally sum all 
+//obtained products. In our case we will have 8-digits: 17032007, so the
+//weight is 1*7 + 1*0 + 1*3 + 1*2 + 1*0 + 1*0 + 1*7 + 7*0 + 7*3 + 7*2 + 
+//7*0 + 7*0 + 7*7 + 0*3 + 0*2 + 0*0 + 0*0 + 0*7 + 3*2 + 3*0 + 3*0 + 3*7
+//+ 2*0 + 2*0 + 2*7 + 0*0 + 0*7 + 0*7 = 144.
+//Your task is to write a program that finds all magic dates: 
+//dates between two fixed years matching given magic weight. 
+//The dates should be printed in increasing order in format 
+//dd-mm-yyyy. We use the traditional calendar (years have 12
+//months, each having 28, 29, 30 or 31 days, etc.). 
+//Years start from 1 January and end in 31 December.
+
+//SOLUTION CHECKED - 100%
+
+class MagicDates
+{
+    static int magicWeight;
+    static int magicCtr = 0;
+
+    static int GetMagicSum(string date)
+    {
+        int magicSum = 0;
+
+        for (int i = 0; i < date.Length; i++)
+        {
+            for (int j = i + 1; j < date.Length; j++)
+            {
+                int x = (int)date[i] - '0';
+                int y = (int)date[j] - '0';
+                magicSum += x * y;
+            }
+        }
+
+        return magicSum;
+    }
+
+    static void Main()
+    {
+        System.Threading.Thread.CurrentThread.CurrentCulture =
+        System.Globalization.CultureInfo.InvariantCulture;
+
+        DateTime startDate = new DateTime();
+        DateTime endDate = new DateTime();
+
+        startDate = startDate.AddYears(int.Parse(Console.ReadLine()) - 1);
+        endDate = endDate.AddYears(int.Parse(Console.ReadLine()) - 1);
+        magicWeight = int.Parse(Console.ReadLine());
+
+        endDate = endDate.AddDays(31 - 1);
+        endDate = endDate.AddMonths(12 - 1);
+
+
+        while (startDate != endDate)
+        {
+            string date = startDate.ToString("MMddyyyy");
+
+            if (GetMagicSum(date) == magicWeight)
+            {
+                Console.WriteLine(startDate.ToString("dd-MM-yyyy"));
+                magicCtr++;
+            }
+
+            startDate = startDate.AddDays(1);
+        }
+
+        if (magicCtr == 0) Console.WriteLine("No");
+    }
+}
---
+++ CCharp Basic/05.ConditionalStatements/17.BitKiller/BitKiller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+//Problem 17.	** – Bit Killer
+//You are given a sequence of bytes. Consider each byte as 
+//sequence of exactly 8 bits.  You are given also a number step.
+//Write a program to remove (kill) all the bits at positions:
+//1, 1 + step, 1 + 2*step, ... Print the output as a sequence 
+//of bytes. In case the last byte have less than 8 bits, add 
+//trailing zeroes at its right end. Bits in each byte are 
+//counted from the leftmost to the rightmost. 
+//Bits are numbered starting from 0.
+
+// SOLUTION CHECKED - 100%
+
+class BitKiller
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int step = int.Parse(Console.ReadLine());
+        int index = 0;
+        int bitCtr = 0;
+        int keeper = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int current = int.Parse(Console.ReadLine());
+
+            for (int bit = 7; bit >= 0; bit--)
+            {
+                if (!((step == 1 && index > 0) || (index % step == 1)))
+                {
+                    int currentBit = (current >> bit) & 1;
+                    keeper <<= 1;
+                    keeper |= currentBit;
+                    bitCtr++;
+                    //string tester = Convert.ToString(keeper, 2).PadLeft(8, '0');
+
+                    if (bitCtr % 8 == 0)
+                    {
+                        Console.WriteLine(keeper);
+                        bitCtr = 0;
+                        keeper = 0;
+                    }
+                }
+                index++;
+            }
+        }
+
+        // printing last byte if necessery
+        if (bitCtr > 0) 
+        {
+            keeper <<= (8 - bitCtr);
+            Console.WriteLine(keeper);
+        }
+    }
+}
